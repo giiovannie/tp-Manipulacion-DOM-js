ad3128c9a91beb9797016084ed9a32717324c447
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-05-31 01:37:13 -0300

    feat(app.js) se agrego un codigo que borra perosonajes del dom y de la base de informacion de las cards , 'falta mejorarlo aun tiene errores'

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ const personajes = [
     imagen: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/5-abraxas.jpg"
   }
 ];
+let perosnajesCopia = [...personajes]
 
 const contenedor = document.querySelector("#cards-grid") // div -row
 const input = document.getElementById("busqueda"); // input 
@@ -49,7 +50,7 @@ function mostrarPersonajes(mlp) {
   mlp.forEach(personaje => {
     contenedor.innerHTML += `
       <div class="col-3">
-        <div class="card">
+        <div class="card" data-id="${personaje.id}">
           <img src="${personaje.imagen}" class="card-img-top" alt="${personaje.nombre}">
           <div class="card-body">
             <h5 class="card-title">${personaje.nombre}</h5>
@@ -68,7 +69,7 @@ function buscarPersonajes(per,pb){
   
   console.log(coincidencia);
 }
-
+                      //persona,nombre,url
 function cargarPersonaje(p,n,u){
   datos = {
     id: p.length + 1,
@@ -82,35 +83,51 @@ function cargarPersonaje(p,n,u){
   return datos
 }
 
+//carga perosnajes al cargar la pagina
 window.addEventListener("load", ()=>{
   mostrarPersonajes(personajes)
 })
 
-
+//accion que buscaun persnaje
 btnEnviar.addEventListener("click", ()=>{
   busqueda = input.value.replace(/\s/g, "").toLowerCase();
   buscarPersonajes(personajes,busqueda)
   mostrarPersonajes(coincidencia)
 })
 
-
+//accion que abre una ventana modal
 btnModal.addEventListener("click", ()=>{
   form.classList.remove("d-none");
 })
 
+//accion que guarda y crea un personaje
 formPersonaje.addEventListener("submit", (e)=>{
   e.preventDefault()
-  
   cargarPersonaje(personajes,inputName.value,inputUrl.value)
-  
   form.classList.add("d-none")
-
   formPersonaje.reset()
-  
   mostrarPersonajes(personajes)
   
 })
 
+//accion que cierra la ventana modal
 btnCancelar.addEventListener("click", ()=>{
     form.classList.add("d-none")
 })
+
+contenedor.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-primary")) {
+    const card = e.target.closest(".card");
+      console.log(card);
+
+    let valorID = card.getAttribute("data-id")
+    console.log(valorID);
+
+    let idNum = Number(valorID)
+    console.log(typeof idNum);
+    
+    perosnajesCopia = perosnajesCopia.filter(p => p.id !== idNum)
+    console.log(perosnajesCopia);
+    mostrarPersonajes(perosnajesCopia)
+  }
+});
